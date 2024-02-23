d3bd0497fdd9a79ff46692268632672da2babf07
prasanshatamang <tamangprasansha291@gmail.com>
2024-02-23 22:19:28 +0545

    pages section added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return <section className='section'>About</section>
 };
 
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return <section className='section'>
+    Contact
+  </section>
 };
 
 export default Contact;
