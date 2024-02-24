722afd789be80a2296619a69d1396a6fcc69aa99
prasanshatamang <tamangprasansha291@gmail.com>
2024-02-24 22:20:20 +0545

    home,about,header updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Photo from '../img/about/photo1.png'
+import { Link } from 'react-router-dom';
 
 const About = () => {
-  return <section className='section'>About</section>
+  return( 
+  <section className='section'>
+    <div className='container mx-auto h-full relative'>
+      <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-20 lg:px-20'>
+        <div className='flex-1 max-h-96 lg:max-h-1/4 order-2 lg:order-none overflow-hidden'>
+          <img src={Photo} alt='' className='lg:ml-10 lg:w-80'/>
+        </div>
+        <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-0 flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1 mb-8'>About Me</h1>
+          <p className='mb-12 max-w-sm'>Hello, I am Prasansha Tamang. Currently a student studying at Suunway Business college affilated with BCU(UK)
+          <br/>
+          Despite being enrolled in datascience, I am quite interested in design and development.
+          </p>
+          <Link to={'/portfolio'} className='btn'>View My Work</Link>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  )
 };
 
 export default About;
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import Photo from '../img/home/photo01.jpg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <section className='section '>Home
+  return (
+  <section className='section '>
+    <div className='container mx-auto h-full relative'>
+      <div className='flex flex-col justify-center'>
+        <div className='w-full pt-36 pb-14 lg:pt-40 lg:pb-0 lg:pl-20  lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+          <h2 className='h1'>
+          Designer <br/> & Developer
+          </h2>
+          <p className='text-[26px] lg:text-[30px] font-primary mb-4 lg-mb-12'>Open to Free-lancing!</p>
+          <Link to={'/contact'} className='btn mb-[30px]'>hire me</Link>
+
+        </div>
+        <div className='flex justify-end max-h-100 lg:max-h-max'>
+          <div className='relative overflow-hidden lg:-right-30'>
+            <img src={Photo} alt='' className='md:h-[100vh]'/>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </section>
+  )
 };
 
 export default Home;
